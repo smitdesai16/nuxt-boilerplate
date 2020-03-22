32983379a1db1cac40ad7e2fe1822e242a218fcd
Smit Desai <smit.desai16@gmail.com>
2020-03-22 13:11:03 -0500

    Adding bootstrap

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   // add your custom rules here
   rules: {
 	'no-tabs': ['error', { allowIndentationTabs: true }],
-	'vue/html-indent': ['error', 'tab']
+	'vue/html-indent': ['error', 'tab'],
+	'indent': ['error', 'tab']
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,31 @@
 
 export default {
-  mode: 'universal',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-  /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-    '@nuxt/typescript-build',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
-  ],
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend () {
-    }
-  }
+	mode: 'universal',
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	loading: { color: '#fff' },
+	css: [
+		'~/node_modules/bootstrap/dist/css/bootstrap.min.css'
+	],
+	plugins: [
+	],
+	buildModules: [
+		'@nuxt/typescript-build',
+		'@nuxtjs/stylelint-module'
+	],
+	modules: [
+	],
+	build: {
+		extend () {
+		}
+	}
 }

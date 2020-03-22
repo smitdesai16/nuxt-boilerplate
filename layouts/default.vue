97de104a47175549c5392f7bd4d07c0c6bc33231
Smit Desai <smit.desai16@gmail.com>
2020-03-22 13:56:53 -0500

    Adding vue-property-decorator

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,3 +3,12 @@
 		<nuxt />
 	</div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class DefaultLayout extends Vue {
+
+}
+</script>

4026a7e3cb0678bb2c317c0a98b28b051f8212b7
Smit Desai <smit.desai16@gmail.com>
2020-03-22 13:06:05 -0500

    Fixing tab lint

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="title">
-        nuxt-boilerplate
-      </h1>
-      <h2 class="subtitle" />
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div>
+			<h1 class="title">
+				nuxt-boilerplate
+			</h1>
+			<h2 class="subtitle" />
+			<div class="links">
+				<a
+					href="https://nuxtjs.org/"
+					target="_blank"
+					class="button--green"
+				>
+					Documentation
+				</a>
+				<a
+					href="https://github.com/nuxt/nuxt.js"
+					target="_blank"
+					class="button--grey"
+				>
+					GitHub
+				</a>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">

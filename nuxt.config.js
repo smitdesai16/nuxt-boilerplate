a21d67f614f79d09b3b06710a1facaf0d9f95eb7
Smit Desai <smit.desai16@gmail.com>
2020-03-23 01:50:16 -0500

    Adding theme_color meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,8 @@ export default {
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
 			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-			{ name: 'theme-color', content: '#f00' }
+			{ name: 'theme-color', content: '#f00' },
+			{ name: 'theme_color', content: '#f00' }
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },

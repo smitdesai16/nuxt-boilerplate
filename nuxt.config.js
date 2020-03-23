49cd4a3d2139cf3f575536931a61c45b82ccc2a8
Smit Desai <smit.desai16@gmail.com>
2020-03-23 01:48:05 -0500

    Fixing theme-color meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,8 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+			{ name: 'theme-color', content: '#f00' }
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },

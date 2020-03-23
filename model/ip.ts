ce5a0d4ff7ca60839443d4cf4a73782fdc034e54
Smit Desai <smit.desai16@gmail.com>
2020-03-23 01:32:10 -0500

    Adding model and try catch block on axios call

---
+++ model/ip.ts
@@ -0,0 +1,7 @@
+export default class IP {
+	address: string
+
+	constructor(address: string) {
+		this.address = address
+	}
+}
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-12">
 				<h1>
-					Index {{ ip }}
+					Index {{ address }}
 				</h1>
 			</div>
 		</div>
@@ -12,12 +12,17 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import IP from '@/model/ip'
 
 @Component
 export default class IndexPage extends Vue {
 	async asyncData({ $axios } : { $axios: any; }) {
-		const ip = await $axios.$get('http://icanhazip.com')
-		return { ip }
+		try {
+			const address = await $axios.$get('http://icanhazip.com', { timeout: 1000 })
+			return new IP(address)
+		} catch (exception) {
+			return new IP('not found')
+		}
 	}
 }
 </script>

94e506a35861936e6a6a96eb641738fd3d41a48b
Smit Desai <smit.desai16@gmail.com>
2020-04-02 18:57:35 -0500

    Fixing title in ga

--- middleware/ga_middleware.ts
+++ middleware/ga_middleware.ts
@@ -2,6 +2,7 @@ export default function({ route }: { route:any }) {
 	if (process.client) {
 		window.ga('create', process.env.GA_ID, 'auto')
 		window.ga('set', 'page', route.fullPath)
+		window.ga('set', 'title', route.name)
 		window.ga('send', 'pageview')
 	}
 }
